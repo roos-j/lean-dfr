a07dbf41a741b9d4803c71b9305ba2e9c0742996
Marco Fraccaroli <marco.g.fraccaroli@gmail.com>
2026-09-11 09:50:00 -0400

    test commit

--- DFR/Theorems.lean
+++ DFR/Theorems.lean
@@ -8,3 +8,9 @@ Here we state the main theorems of the paper.
 The proofs are in the `Auto` folder.
 
 -/
+
+theorem my_theorem (a b : ℕ) (hab : a < b) :
+    a ≠ b := by
+  sorry
+
+#print axioms my_theorem
